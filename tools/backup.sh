be8954ed69497a3e859cbab770fe0e91134118b6
minecraft server <minecraft@seattleacademy.org>
2020-06-07 13:17:16 -0700

    Update backup.sh

--- tools/backup.sh
+++ tools/backup.sh
@@ -3,15 +3,16 @@
 function rcon {
   /opt/minecraft/tools/mcrcon/mcrcon -H 127.0.0.1 -P 25575 -p '#1234567890' "$1"
 }
-
+thedate=$(date +%F-%H-%M)
 rcon "save-off"
 rcon "save-all"
-tar -cvpzf /opt/minecraft/backups/server-$(date +%F-%H-%M).tar.gz /opt/minecraft/server
+tar -cvpzf /opt/minecraft/backups/server-"$thedate".tar.gz /opt/minecraft/server
 rcon "save-on"
-rm -rf /opt/minecraft/oldmathworld
-mv /opt/minecraft/mathworld /opt/minecraft/oldmathworld
-scp -P 3389 -r minecraft@math.seattleacademy.org:/opt/minecraft/server/world /opt/minecraft/mathworld
+#rm -rf /opt/minecraft/oldmathworld
+#mv /opt/minecraft/mathworld /opt/minecraft/oldmathworld
+#scp -P 3389 -r minecraft@math.seattleacademy.org:/opt/minecraft/server/world /opt/minecraft/mathworld
+scp -P 3389 -r /opt/minecraft/backups/server-"$thedate".tar.gz minecraft@math.seattleacademy.org:/opt/minecraft/backups/minecraftserver-"$thedate".tar.gz
 
 ## Delete older backups
-find /opt/minecraft/backups/ -type f -mtime +700 -name '*.gz' -delete
+find /opt/minecraft/backups/ -type f -mtime +7 -name '*.gz' -delete
 
